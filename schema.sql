94b3f544e631b6c08aa2efe1e47f36d88b76b261
tessconnell14 <152419320+tessconnell14@users.noreply.github.com>
2024-04-12 17:29:55 +0100

    Trying to display tables on schema
    When opened the file in workbench says 'unconnected', trying to fix

--- schema.sql
+++ schema.sql
@@ -1,11 +1,3 @@
 USE new_schema;
 
-ALTER TABLE bookings
-ADD COLUMN booking_date DATETIME;
-
-ALTER TABLE bookings
-ADD CONSTRAINT fk_booking__date
-FOREIGN KEY (booking__date)
-REFERENCES availability(booking__date);
-
 SELECT * FROM bookings
